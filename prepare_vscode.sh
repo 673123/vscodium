bff7f53f1eb8226110e36ff36ec2efac61422db2
673123 <187576858+673123@users.noreply.github.com>
2026-09-24 11:32:46 -0400

    Disable update

--- prepare_vscode.sh
+++ prepare_vscode.sh
@@ -51,12 +51,12 @@ setpath "product" "tipsAndTricksUrl" "https://go.microsoft.com/fwlink/?linkid=85
 setpath "product" "twitterUrl" "https://go.microsoft.com/fwlink/?LinkID=533687"
 
 if [[ "${DISABLE_UPDATE}" != "yes" ]]; then
-  setpath "product" "updateUrl" "https://raw.githubusercontent.com/VSCodium/versions/refs/heads/master"
+  # setpath "product" "updateUrl" "https://raw.githubusercontent.com/VSCodium/versions/refs/heads/master"
 
   if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
-    setpath "product" "downloadUrl" "https://github.com/VSCodium/vscodium-insiders/releases"
+    # setpath "product" "downloadUrl" "https://github.com/VSCodium/vscodium-insiders/releases"
   else
-    setpath "product" "downloadUrl" "https://github.com/VSCodium/vscodium/releases"
+    # setpath "product" "downloadUrl" "https://github.com/VSCodium/vscodium/releases"
   fi
 
   # if [[ "${OS_NAME}" == "windows" ]]; then
